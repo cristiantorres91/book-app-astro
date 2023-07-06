1f413f7d07bd1b78e9e9a3d4c52dd1328e865e9e
Cristian Torres <ctorres@codisa.com>
2023-07-06 09:57:20 -0600

    first commit

---
+++ src/data/books.js
@@ -0,0 +1,34 @@
+export const books = [
+	{
+		id: 'clean-code',
+		title: 'Clean Code: A Handbook of Agile Software Craftsmanship',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg',
+		author: 'Robert C. Martin',
+		opinion:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque ut iusto exercitationem rem delectus? Eos veritatis obcaecati quo doloremque, facere, delectus corporis est animi qui ratione suscipit temporibus modi architecto.',
+	},
+	{
+		id: 'the-clean-coder',
+		title: 'The Clean Coder: A Code of Conduct for Professional Programmers',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg',
+		author: 'Robert C. Martin',
+		opinion:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque ut iusto exercitationem rem delectus? Eos veritatis obcaecati quo doloremque, facere, delectus corporis est animi qui ratione suscipit temporibus modi architecto.',
+	},
+	{
+		id: 'the-pragmatic-programmer',
+		title: 'The Pragmatic Programmer: From Journeyman to Master',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg',
+		author: 'Robert C. Martin',
+		opinion:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque ut iusto exercitationem rem delectus? Eos veritatis obcaecati quo doloremque, facere, delectus corporis est animi qui ratione suscipit temporibus modi architecto.',
+	},
+	{
+		id: 'code-complete',
+		title: 'Code Complete',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1396837641i/4845.jpg',
+		author: 'Robert C. Martin',
+		opinion:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque ut iusto exercitationem rem delectus? Eos veritatis obcaecati quo doloremque, facere, delectus corporis est animi qui ratione suscipit temporibus modi architecto.',
+	},
+];
